aa8eb90384790cae5302efc24e79e9209876a424
Thiago <thiagolinog8@gmail.com>
2022-09-13 13:07:21 -0300

    Lame

--- src/screens/ToDo.tsx
+++ src/screens/ToDo.tsx
@@ -1,12 +1,14 @@
 import AddTask from '../components/AddTask'
 import Search from '../components/Search'
 import { Tasks } from '../components/Tasks'
+import { useStore } from '../store'
 
 const ToDo = () => {
+	const { tasks } = useStore()
 	return (
 		<>
 			<h1>To Do List</h1>
-			<Search />
+			{!!tasks.length && <Search />}
 			<AddTask />
 			<Tasks />
 		</>
